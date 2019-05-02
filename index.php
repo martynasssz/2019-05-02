b90dbb5dd5fbb77ed360ce2b88cf8a4fa9091b3a
martynasssz <martynasssk@gmail.com>
2019-05-02 18:00:01 +0300

    2019-05-02pirmas

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php print rand(0,255). '. '. rand(0,255). '. '. rand(0,255);?>
+
+
+
+<html>
+<head>
+    <title> <?php print 'PHP lydės ir '.date('Y-m-d',strtotime('+'.rand(0,10).'years'));?> </title>
+</head>
+
+
+<body style="background-color: rgb(<?php print rand(0,255) . "," . rand(0,255) . "," . rand(0,255); ?>)"
+<h1 style="font-size:(<?php print rand(10;50).'px';?>
+
+</h1>
+
+<p>  <?php print 'VD. Trumpas nebebus prezidentu '.date('2021-m-d',strtotime('+'.rand(2,12).'years'));?></p>
+
+<?php print date(rand(2021,2031).'-m-d');?>
+
+
+<!--random funkcijos naudojimas-->
+
+</body>
+
+
+</html>
