a6116cd97fdeb0fd0abc054ca8af1f52d0a2cd9a
martynasssz <martynasssk@gmail.com>
2019-05-02 19:21:58 +0300

    2019-05-02 antra

--- index.php
+++ index.php
@@ -1,26 +1,44 @@
-<?php print rand(0,255). '. '. rand(0,255). '. '. rand(0,255);?>
-
-
+<?php
+$kintamasis =rand(0,1);
+//
+//if($kintamasis <1 ){
+//    print 'sauleta';
+//}  else
+//{ print 'debesuota';
+//
+//}
+?>
 
 <html>
 <head>
-    <title> <?php print 'PHP lydės ir '.date('Y-m-d',strtotime('+'.rand(0,10).'years'));?> </title>
+    <style>
+      .sauleta {
+          background-color: yellow;
+          height: 100px;
+          width: 100px;
+      }
+      .debesuota {
+          background-color: blue;
+          height: 100px;
+          width: 100px;
+      }
+    </style>
+
 </head>
+<body>
+<?php if ($kintamasis<1):?>
+
+<div class="sauleta"> </div>
+
+<?php else: ?>
+<div class="debesuota"> </div>
 
 
-<body style="background-color: rgb(<?php print rand(0,255) . "," . rand(0,255) . "," . rand(0,255); ?>)"
-<h1 style="font-size:(<?php print rand(10;50).'px';?>
-
-</h1>
-
-<p>  <?php print 'VD. Trumpas nebebus prezidentu '.date('2021-m-d',strtotime('+'.rand(2,12).'years'));?></p>
-
-<?php print date(rand(2021,2031).'-m-d');?>
+<?php endif?>
 
 
-<!--random funkcijos naudojimas-->
 
 </body>
 
-
 </html>
+
